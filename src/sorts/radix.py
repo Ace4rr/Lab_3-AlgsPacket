cbc8032ec2b72c2ab0b66b9d82a1775b8c3c5fb4
Ace4rr <f4dor2007@gmail.com>
2025-12-03 21:52:20 +0200

    Readme

---
+++ src/sorts/radix.py
@@ -0,0 +1,24 @@
+from logger import logger
+
+def radix_sort(arr, base=10):
+    logger.debug(f"radix_sort input:{arr}")
+    if not arr:
+        return arr
+    max_val=max(arr)
+    expas=1
+    a=arr[:]
+    while max_val//exp>0:
+        output=[0]*len(a)
+        count=[0]*base
+        for i in a:
+            count[(i//expas)%base]+=1
+        for i in range(1, base):
+            count[i]+=count[i-1]
+        for i in reversed(a):
+            index=(i//expas)%base
+            count[index]-=1
+            output[count[index]]=i
+        a=output
+        expas*=base
+    logger.debug(f"radix_sort output: {a}")
+    return a
